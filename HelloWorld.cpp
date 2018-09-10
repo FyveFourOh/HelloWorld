e49d6e846ab7bcf444e75ff3eebef1a7271c53fd
FyveFourOh <38377205+FyveFourOh@users.noreply.github.com>
2018-09-09 21:22:39 -0400

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int myAge = 18;
+
+// Prints My name is Nick and I am 18 years old!
+int main()
+{
+  std::cout << "My name is Nick and I am " << myAge << " years old!";
+
+  std::cin.get();
+
+  return 0;
+}
